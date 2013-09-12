4ba6eae76470a6a7c3ec34b8559a8bb9b84a5368
Sean Seefried <sean.seefried@gmail.com>
2013-09-12 15:08:27 +1000

    Initial commit of Nip it in the Bud: A game about exponential growth

---
+++ game.js
@@ -0,0 +1,150 @@
+var Game = function ($, Box2D) {
+
+  // sseefried: I don't want to use magic numbers in my code, so I'm defining the
+  //   width and height for now. FIXME: get this directly from the canvas.
+  var widthInPixels  = 640;
+  var heightInPixels = 480;
+
+  var width = 40;
+  var height = heightInPixels/widthInPixels * width;
+
+  var scale = widthInPixels/width; // world is 40m wide
+
+  var germSize = 2;
+
+  var stepsInSecond = 30;
+  var timeStep = 1/stepsInSecond;
+
+  var doublingPeriod = 3; // in seconds
+
+  var b2 = Box2DWorld(Box2D);
+
+  var velocityIterations = 8;
+  var positionIterations = 3;
+
+ var setupDebugDraw = function() {
+    var debugDraw = new b2.DebugDraw();
+    context = document.getElementById('canvas').getContext('2d');
+
+    // rshankar: Use this canvas context for drawing the debugging screen
+    debugDraw.SetSprite(context);
+    debugDraw.SetDrawScale(scale);
+    // rshankar: Fill boxes with alpha transparency of 0.3
+    debugDraw.SetFillAlpha(0.3);
+    debugDraw.SetLineThickness(1.0);
+
+    // rshankar: Display all shapes and joints
+    debugDraw.SetFlags(b2.DebugDraw.e_shapeBit | b2.DebugDraw.e_jointBit);
+    b2.world.SetDebugDraw(debugDraw);
+  };
+
+  //
+  // Draws a beaker centred at (x,y) of size (width,height)
+  //
+  var createBeaker = function(o) {
+    var baseShape = new b2.PolygonShape,
+        sideShape = new b2.PolygonShape;
+
+    baseShape.SetAsBox(o.width/2,     o.wallWidth/2);
+    sideShape.SetAsBox(o.wallWidth/2, (o.height - o.wallWidth)/2);
+
+    b2.createBody({x: o.x - (o.width - o.wallWidth)/2, y: o.y}, [ { shape: sideShape}]);
+    b2.createBody({x: o.x + (o.width - o.wallWidth)/2, y: o.y}, [ { shape: sideShape}]);
+    b2.createBody({x: o.x, y: o.y + o.height/2}, [ { shape: baseShape }]);
+
+  };
+
+  var createGerm = function(o) {
+    var body = b2.createDynamicBody({x: o.x, y: o.y, angularDamping: 0.7},
+                         [{ density: 1.0,
+                            friction: 1.0,
+                            restitution: 0.0, 
+                            shape: new b2.CircleShape(germSize) }]);
+    body.SetUserData({ type: "germ"});
+    return body;
+
+  }
+
+  var start = function() {
+
+    var gameState = { step: 0, failed: false },
+        context = $('#canvas')[0].getContext("2d");
+
+    createBeaker({x: 20, y: 19.5, width: 36, height: 20, wallWidth: 1});
+    createGerm({x: width/2, y: 25});
+
+    var mouseHandler = function(selector) { 
+      var offset = $(selector).offset();
+      return function(e) {
+        var aabb = new b2.AABB(), x = (e.pageX - offset.left)/scale, 
+                                  y = (e.pageY - offset.top)/scale,
+            delta = 0.1, b, userData, body;
+
+            console.log(x,y);
+
+        aabb.lowerBound = { x: x-delta, y: y-delta};
+        aabb.upperBound = { x: x+delta, y: y+delta};
+        b2.world.QueryAABB(function(data) {
+          var body = data.GetBody();
+          if (body.GetUserData()) {
+            b2.world.DestroyBody(body);
+            body = null;
+          }
+        }, aabb);
+
+      };
+    };
+
+    $('#canvas').click(mouseHandler('#canvas'));
+
+    var multiplyGerms = function() {
+      var newPos;
+      for (b = b2.world.GetBodyList(); b; b = b.GetNext()) {
+        if (userData = b.GetUserData()) { 
+
+          if (b.GetPosition().y < 10) {
+            gameState.failed = true;
+          }
+
+          if (gameState.step === (doublingPeriod*stepsInSecond - 1)) {
+            newPos = b.GetPosition();
+            newPos.x += 0.1;
+            createGerm(newPos);
+          }
+        }
+      }
+    };
+
+    var failedMessage = function() {
+      context.fillStyle = "red";
+      context.font = "bold 64px Helvetica";
+      context.textAlign = "center";
+      context.fillText("FAILED!", widthInPixels/2, heightInPixels/5)
+
+    }
+
+    var animate = function() {
+      b2.world.Step(timeStep, velocityIterations, positionIterations);
+      gameState.step = (gameState.step + 1) % (doublingPeriod * stepsInSecond);
+      b2.world.ClearForces();
+      b2.world.DrawDebugData();
+      multiplyGerms();
+      if ( !gameState.failed ) { 
+        setTimeout(function() { animate(); }, timeStep*1000);
+      } else {
+        failedMessage();
+      }
+    };
+
+    setupDebugDraw();
+    animate();
+
+  };
+
+  return ({
+    start: start
+  })
+
+};
+
+var game = Game(jQuery, Box2D);
